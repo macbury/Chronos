df47db6c90f203eaa944dfb48e5cfcb0308ba5c9
macbury <macbury@gmail.com>
2011-03-24 17:52:46 +0100

    Golden ticket

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chronos::Application.config.secret_token = '81c2ae124b251834e608e45b8f6cc847cc5fd3411437437b27fd00f9a0b5ed14fa3219c456cba689d448d15bc1a7c9f5177775c5376c2346643592c9ddcd4362'
