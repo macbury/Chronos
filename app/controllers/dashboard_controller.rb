f4501fdd1190ebdd2ae5390b3af40d9c8c88adff
macbury <macbury@gmail.com>
2011-04-06 22:50:13 +0200

    interface

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @updates = self.current_user.updates.includes(:links).all
+    @updates = self.current_user.updates.includes(:links).order("publish_at DESC").all
   end
 end
