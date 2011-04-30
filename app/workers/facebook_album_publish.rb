3b5e2759d971d79fb1ea9bbcce7fb8b3ca537f35
macbury <macbury@gmail.com>
2011-04-30 09:34:14 +0200

    facebook album and photo publish

--- app/workers/facebook_album_publish.rb
+++ app/workers/facebook_album_publish.rb
@@ -6,7 +6,27 @@ class FacebookAlbumPublish < StreamPublish
     if resp["id"]
       link.uid = resp["id"]
       link.save
+      
+      photos = link.stream.streamable.photos.all
+      total = photos.size
+      done = 0
+      
+      photos.each do |photo|
+        begin
+          self.upload_picture(photo)
+          done += 1
+          self.progress((done * 100 / total).round)
+        rescue Exception => e
+        
+        end
+      end
     end
+  end
+  
+  def upload_picture(photo)
+    graph = Koala::Facebook::GraphAPI.new(link.social_account.token)
+    
+    resp = graph.put_picture(photo.image.path, link.uid)
   end
 end
 
--- app/workers/facebook_photo_publish.rb
+++ app/workers/facebook_photo_publish.rb
@@ -1,26 +1,6 @@
 class FacebookPhotoPublish < StreamPublish
   def perform
-    graph = Koala::Facebook::GraphAPI.new(link.social_account.token)
 
-    resp = graph.put_object(link.social_account.uid, "albums", link.stream.streamable.to_facebook)
-    if resp["id"]
-      link.uid = resp["id"]
-      link.save
-      
-      photos = link.stream.streamable.photos.all
-      total = photos.size
-      done = 0
-      
-      photos.each do |photo|
-        begin
-          self.upload_picture(photo)
-          done += 1
-          self.progress((done * 100 / total).round)
-        rescue Exception => e
-        
-        end
-      end
-    end
   end
   
   def upload_picture(photo)
