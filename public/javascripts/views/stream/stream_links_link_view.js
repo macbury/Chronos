6fe091d84d1b168a33f03f03d2ac91a36d8fe033
Arkadiusz Buras <macbury@GLaDOS.(none)>
2011-05-01 23:22:06 +0200

    progress

--- public/javascripts/views/stream/stream_links_link_view.js
+++ public/javascripts/views/stream/stream_links_link_view.js
@@ -7,7 +7,7 @@ $(function(){
     },
     
     initialize: function() {
-      _.bindAll(this, 'render', "refresh");
+      _.bindAll(this, 'render', "refresh", "canvas", "render_progress");
       this.model.bind('refresh', this.render);
       this.model.bind('change', this.refresh);
     },
@@ -16,9 +16,40 @@ $(function(){
       $(this.el).html(Haml.render(JST.stream_links, { locals: {link: this.model} }));
     },
     
+    canvas: function() {
+      return this.$('canvas')[0].getContext('2d');
+    },
+    
+    render_progress: function() {
+      var progress = this.model.get("progress");
+      if(progress > 0) {
+        progress = progress / 100;
+      }
+      var ctx = this.canvas();
+      ctx.save();
+      ctx.clearRect(0,0,32,32);
+      ctx.translate(16,16);
+      ctx.scale(2,2);
+
+      ctx.rotate(-Math.PI/2);
+      ctx.lineWidth = 2;
+      ctx.lineCap = "square";
+      
+      ctx.save();
+      ctx.strokeStyle = "#D7D7D7";
+      ctx.beginPath();
+      
+      ctx.arc(0,0, 4,0,progress*(Math.PI*2),false);
+      
+      ctx.stroke();
+      ctx.restore();
+    },
+    
     render: function() {
       $(this.el).html(Haml.render(JST.stream_links, { locals: {link: this.model} }));
       $(this.el).addClass(this.model.get("social_account").type_name).attr("id","stream_link_"+this.model.get("id"));
+      this.render_progress();
+      
       return this;
     },
   });
